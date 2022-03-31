1a4c50d7d3505c9f1a5bac4ef98ce3693a890c62
coderamey <101514346+coderamey@users.noreply.github.com>
2022-03-31 13:23:53 +0530

    Add files via upload

---
+++ adding two number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    int sum;
+     clrscr();
+     printf("enter two no : ");
+     scanf("%d%d",&a,&b);
+
+     sum= a+b;
+     printf("sum = %d ",sum);
+     getch();
+}
